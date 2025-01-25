320dea645f8c6a817bd21866c13673b58fe2a67d
NicklasMiller <nicklas.a.miller@gmail.com>
2025-01-25 00:35:39 -0600

    Coins can be collected
    TODO: Make coin counter work

---
+++ collect_coin.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+signal coin_collected
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Coin Body Entered")
+	emit_signal("coin_collected")
+	queue_free()
