77f83e0c98e117e3867cfcf5ce0b21462b6afcc5
GreenGuyBlueGuy <joelittle.ajax@gmail.com>
2025-01-25 10:24:42 -0600

    Game Menu Added

---
+++ UI Elements/game_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var phrases =  [
+"Time is a brisk wind, for each hour it brings something new. - Paracelsus",
+"Time, because it is so fleeting, time, because it is beyond recall, is the most precious of human goods and to squander it is is the most delicate form of dissipation in which man can indulge. - W. Somerset Maugham", 
+"But meanwhile, time flies; it flies never to be regained. - Virgil" ,
+"Time moves slowly but passes quickly. - Alice Walker", 
+"Those who make the worst use of their time are the first to complain of its brevity. - Jean de La Bruyère", 
+"Seize the day, then let it go. - Marty Rubin", 
+"Everybody knows that once you passed it, you can't go back. - Claire Coo"
+]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Panel/Start.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_dread_pressed() -> void:
+	var random_phrase = phrases[randi() % phrases.size()]
+	$DreadPhrase.text = random_phrase
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
