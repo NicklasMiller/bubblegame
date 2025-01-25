35d0ab9f313c79e1f51de6827dcb7fea99b0e5c7
GreenGuyBlueGuy <joelittle.ajax@gmail.com>
2025-01-25 13:50:13 -0600

    added a Test Level 1

---
+++ Levels/level_1.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var narrator_positive = [
+	"That's right move closer to the bubble wand",
+	"Isn't this fun?",
+	"I could do this FOREVER..."
+	
+]
+
+var narrator_negative = [
+	"Why are you going that way?",
+	"Silly bubble that's the wrong way.",
+	"Why would you do that????",
+	"That's ok, I've got plent of more bubble soap"
+	
+	
+]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
